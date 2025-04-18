8b041e6dbf430302583486307104ef62bdfff493
c2neyt <c2neyt@proton.me>
2025-04-18 10:16:06 +0300

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  "discordId": "535436474774978563",
+  "discordId": "269625962423910400",
   "apiUrl": "https://camilo404.azurewebsites.net/v1/",
   "webSocketUrl": "wss://api.lanyard.rest/socket"
 };
